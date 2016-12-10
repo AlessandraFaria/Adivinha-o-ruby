142eeb57eea6a0d894582fa7bfadda8282e7e8e2
Alessandra Faria <alessandra.faria.abreu@gmail.com>
2016-12-10 01:14:14 -0200

    Add files via upload

---
+++ maior_ou_menor.rb
@@ -0,0 +1,17 @@
+puts "Jogo da adivinhação"
+puts "Escreva o seu nome ?"
+nome = gets
+puts
+puts
+puts"Bem-vindo " + nome
+puts
+puts
+puts "Escolhendo um número secreto entre 0 e 200..."
+numero_secreto = 88
+puts "Já escolhemos um número secreto , tente adivinhar "
+puts 
+puts 
+puts 
+puts "Primeira tentativa"
+puts "Digite o seu palpite : "
+chute = gets 
