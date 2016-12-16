0054e8d6f06f2b6cbac82ff43f6b2292cfdc9c49
Alessandra Faria <alessandra.faria.abreu@gmail.com>
2016-12-16 00:54:26 -0200

    Update maior_ou_menor.rb

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,19 +1,28 @@
 puts "Jogo da adivinhação"
 puts "Escreva o seu nome ?"
 nome = gets
-puts
-puts
-puts"Bem-vindo " + nome
-puts
-puts
+puts "\n\n"
+puts "Bem-vindo " + nome
+puts "\n\n\n"
+
 puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 88
 puts "Já escolhemos um número secreto , tente adivinhar "
-puts 
-puts 
-puts 
+puts "\n\n\n"
+
 puts "Primeira tentativa"
 puts "Digite o seu palpite : "
 chute = gets
+
 puts "Será que você acertou ?"
-chute.to_i == numero_secreto 
+acertou = chute.to_i == numero_secreto 
+if acertou
+	puts "Acertou!"
+else
+	puts "Errou"
+	menor = chute.to_i < numero_secreto 
+	if maior
+		puts"O número secreto é maior que o escolhido"
+	else 
+		puts "O número secreto é menor que o escolhido"
+end
