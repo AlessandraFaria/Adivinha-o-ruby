5d93d92f99ccbb1e73a81e70f72ff3f67c128ab5
Alessandra Faria <alessandra.faria.abreu@gmail.com>
2016-12-16 01:17:45 -0200

    for

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -10,19 +10,21 @@ numero_secreto = 88
 puts "Já escolhemos um número secreto , tente adivinhar "
 puts "\n\n\n"
 
-puts "Primeira tentativa"
-puts "Digite o seu palpite : "
-chute = gets
+	for tentativas in 1..3
+		puts "Tentativa "+ tentativas.to_s 
+		puts "Digite o seu palpite : "
+		chute = gets
 
-puts "Será que você acertou ?"
-acertou = chute.to_i == numero_secreto 
-if acertou
-	puts "Acertou!"
-else
-	puts "Errou"
-	menor = chute.to_i < numero_secreto 
-	if maior
-		puts"O número secreto é maior que o escolhido"
-	else 
-		puts "O número secreto é menor que o escolhido"
-end
+		puts "Será que você acertou ?"
+		acertou = chute.to_i == numero_secreto 
+		if acertou
+			puts "Acertou!"
+		else
+			puts "Errou"
+			menor = chute.to_i < numero_secreto 
+			if maior
+				puts"O número secreto é maior que o escolhido"
+			else 
+				puts "O número secreto é menor que o escolhido"
+		end
+	end
